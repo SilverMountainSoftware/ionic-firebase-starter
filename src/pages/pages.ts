3912a73275deb68e34a2dd72e36c765f1c143557
jbaima <jkbaima@gmail.com>
2017-07-08 15:31:57 -0500

    Cleanup

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -1,12 +1,10 @@
 import { ListMasterPage } from './list-master/list-master';
-import { SearchPage } from './search/search';
 import { SettingsPage } from './settings/settings';
-import { TabsPage } from './tabs/tabs';
 import { TutorialPage } from './tutorial/tutorial';
 import { MenuPage } from './menu/menu'
 
 // The page the user lands on after opening the app and without a session
-export const FirstRunPage = ListMasterPage; ////TutorialPage;
+export const FirstRunPage = TutorialPage;
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
